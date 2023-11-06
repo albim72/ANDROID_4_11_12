4943120ac62282442848a528c1473b08e8221fbd
albim72 <marcin.albiniak72@gmail.com>
2023-11-06 15:46:16 +0100

    Create Anglo.java

---
+++ DZIEN_1/KALJULATORODLEGLOSCI/Anglo.java
@@ -0,0 +1,13 @@
+package com.marcin.kalkulatorodleglosci;
+
+public class Anglo extends MiaraAnglo implements IPrzeliczniki{
+
+    public Anglo(double odl, double mang) {
+        super(odl, mang);
+    }
+
+    @Override
+    public double policzOdl() {
+        return odl/mang;
+    }
+}
