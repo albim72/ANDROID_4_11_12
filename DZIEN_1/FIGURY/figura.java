d50d8b0886c2b8e5cda3291e97c0114147a79c4f
albim72 <marcin.albiniak72@gmail.com>
2023-11-06 13:57:46 +0100

    Create figura.java

---
+++ DZIEN_1/FIGURY/figura.java
@@ -0,0 +1,13 @@
+package com.marcin.figury;
+
+public abstract class Figura {
+    protected double a;
+    protected double b;
+
+    public Figura(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    public abstract double policzPole();
+}
