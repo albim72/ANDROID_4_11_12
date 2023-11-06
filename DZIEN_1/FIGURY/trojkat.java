764c292441ecd8da750fc17c6e733765edc5159c
albim72 <marcin.albiniak72@gmail.com>
2023-11-06 14:49:53 +0100

    Create trojkat.java

---
+++ DZIEN_1/FIGURY/trojkat.java
@@ -0,0 +1,12 @@
+package com.marcin.figury;
+
+public class Trojkat extends Figura{
+    public Trojkat(double a, double b) {
+        super(a, b);
+    }
+
+    @Override
+    public double policzPole() {
+        return a*b/2;
+    }
+}
