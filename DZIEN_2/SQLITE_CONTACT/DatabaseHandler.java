070fda6ad126ffbfc02811970d20e30f7340c3d4
albim72 <marcin.albiniak72@gmail.com>
2023-11-17 14:06:27 +0100

    Create DatabaseHandler.java

---
+++ DZIEN_2/SQLITE_CONTACT/DatabaseHandler.java
@@ -0,0 +1,36 @@
+package com.marcin.sqlitecontact;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import kotlin.text.UStringsKt;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactsManager";
+    private static final String TABLE_NAME = "contacts";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PH_NO = "phone_number";
+    
+    public DatabaseHandler(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+                KEY_NAME + " TEXT, " + KEY_PH_NO + " TEXT);";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
