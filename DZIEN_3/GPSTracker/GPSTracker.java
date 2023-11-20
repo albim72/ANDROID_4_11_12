81480590fb653cda451824350d813abe23f27270
albim72 <marcin.albiniak72@gmail.com>
2023-11-20 15:14:45 +0100

    Update GPSTracker.java

--- DZIEN_3/GPSTracker/GPSTracker.java
+++ DZIEN_3/GPSTracker/GPSTracker.java
@@ -3,32 +3,36 @@ package com.marcin.gpstracker;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import java.security.PrivateKey;
 
 public class GPSTracker extends Service implements LocationListener {
-    
+
+    private static final Location TODO = null;
     private final Context mContext;
     //flaga statusu GPS
     boolean isGPSEnabled = false;
     //flaga statusu sieci
-    boolean inNetworkEnabled = false;
+    boolean isNetworkEnabled = false;
     //flaga ze statusu GPS
-    boolean cenGetLocation = false;
+    boolean canGetLocation = false;
     Location location;
     double latitude;
     double longitude;
-    
+
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    private static final long   MIN_TIME_BE_UPDATES = 1000*60*1;
-    
+    private static final long MIN_TIME_BE_UPDATES = 1000 * 60 * 1;
+
     protected LocationManager locationManager;
 
     public GPSTracker(Context mContext) {
@@ -36,11 +40,90 @@ public class GPSTracker extends Service implements LocationListener {
         getLocation();
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isGPSEnabled && !isNetworkEnabled) {
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return TODO;
+                    }
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BE_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BE_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this
+                        );
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
     }
+
+    public void stopUsingGPS(){
+        if(locationManager !=null){
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+    public double getLatitude() {
+        if(location!=null){
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLongitude() {
+        if(location!=null){
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    @Nullable
+        @Override
+        public IBinder onBind (Intent intent){
+            return null;
+        }
+
+
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
