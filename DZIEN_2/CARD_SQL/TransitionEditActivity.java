42eab9eb1c82af1c58a8e4cf3584edfb7ae7ccf5
albim72 <marcin.albiniak72@gmail.com>
2023-11-20 11:29:06 +0100

    Update TransitionEditActivity.java

--- DZIEN_2/CARD_SQL/TransitionEditActivity.java
+++ DZIEN_2/CARD_SQL/TransitionEditActivity.java
@@ -1,4 +1,4 @@
-
+package com.marcin.cardssql;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class TransitionEditActivity extends AppCompatActivity {
     private EditText nameEditText;
     private TextView initialTextView;
@@ -23,15 +25,15 @@ public class TransitionEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transition_edit);
 
-        nameEditText = (EditText) findViewById(R.id.name);
-        initialTextView = (TextView) findViewById(R.id.initial);
+        nameEditText = (EditText) findViewById(R.id.name_change);
+        initialTextView = (TextView) findViewById(R.id.initialedit);
         Button update_button = (Button) findViewById(R.id.update_button);
         Button delete_button = (Button) findViewById(R.id.delete_button);
 
         intent = getIntent();
-        String nameExtra = intent.getStringExtra(SampleMaterialActivity.EXTRA_NAME);
-        String initialExtra = intent.getStringExtra(SampleMaterialActivity.EXTRA_INITIAL);
-        int colorExtra = intent.getIntExtra(SampleMaterialActivity.EXTRA_COLOR, 0);
+        String nameExtra = intent.getStringExtra(MainActivity.EXTRA_NAME);
+        String initialExtra = intent.getStringExtra(MainActivity.EXTRA_INITIAL);
+        int colorExtra = intent.getIntExtra(MainActivity.EXTRA_COLOR, 0);
 
         nameEditText.setText(nameExtra);
         nameEditText.setSelection(nameEditText.getText().length());
@@ -48,7 +50,7 @@ public class TransitionEditActivity extends AppCompatActivity {
                     // W kontrolce initialTextView zapisujemy pierwszą literę z kontrolki nameEditText i aktualizujemy
                     // informacje dodatkowe w intencji.
                     initialTextView.setText(String.valueOf(s.charAt(0)));
-                    intent.putExtra(SampleMaterialActivity.EXTRA_UPDATE, true);
+                    intent.putExtra(MainActivity.EXTRA_UPDATE, true);
                 }
             }
 
@@ -69,9 +71,9 @@ public class TransitionEditActivity extends AppCompatActivity {
                 if (TextUtils.isEmpty(text)) {
                     Toast.makeText(getApplicationContext(), "Podaj prawidłowe imię", Toast.LENGTH_SHORT).show();
                 } else {
-                    intent.putExtra(SampleMaterialActivity.EXTRA_UPDATE, true);
-                    intent.putExtra(SampleMaterialActivity.EXTRA_NAME, String.valueOf(nameEditText.getText()));
-                    intent.putExtra(SampleMaterialActivity.EXTRA_INITIAL, String.valueOf(nameEditText.getText().charAt(0)));
+                    intent.putExtra(MainActivity.EXTRA_UPDATE, true);
+                    intent.putExtra(MainActivity.EXTRA_NAME, String.valueOf(nameEditText.getText()));
+                    intent.putExtra(MainActivity.EXTRA_INITIAL, String.valueOf(nameEditText.getText().charAt(0)));
 
                     setResult(RESULT_OK, intent);
                     supportFinishAfterTransition();
@@ -82,7 +84,7 @@ public class TransitionEditActivity extends AppCompatActivity {
         delete_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent.putExtra(SampleMaterialActivity.EXTRA_DELETE, true);
+                intent.putExtra(MainActivity.EXTRA_DELETE, true);
 
                 setResult(RESULT_OK, intent);
                 supportFinishAfterTransition();
