8a518e63a8be53fb3f8a9852d4a1bf190bb6e2bc
albim72 <marcin.albiniak72@gmail.com>
2023-11-20 14:12:34 +0100

    Update MainActivity.java

--- DZIEN_3/SHAREDPREF/MainActivity.java
+++ DZIEN_3/SHAREDPREF/MainActivity.java
@@ -2,23 +2,56 @@ package com.marcin.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     private static final String PREFERENCES_NAME = "myPreferences";
     private static final String PREFERENCES_TEXT_FIELD = "textField";
     private EditText etSave;
     private Button btnSave;
     private SharedPreferences preferences;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preferences = getSharedPreferences(PREFERENCES_NAME, Activity.MODE_PRIVATE);
+        etSave = (EditText) findViewById(R.id.etSave);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        initButtonOnClick();
+        restoreData();
+    }
+
+    private void saveData(){
+        SharedPreferences.Editor preferencesEditor = preferences.edit();
+        String editTextData = etSave.getText().toString();
+        preferencesEditor.putString(PREFERENCES_TEXT_FIELD, editTextData);
+        preferencesEditor.commit();
+    }
+
+    private void restoreData(){
+        String textFromPreferences = preferences.getString(PREFERENCES_TEXT_FIELD,"");
+        etSave.setText(textFromPreferences);
+    }
+
+    private void showToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    private void initButtonOnClick(){
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                showToast("Data save!!!");
+            }
+        });
     }
 }
