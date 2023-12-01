cc4b7fa6c3df969f9bdc0290b1f0e81c6211e993
albim72 <marcin.albiniak72@gmail.com>
2023-12-01 09:13:57 +0100

    Create AutoStartUp.java

---
+++ DZIEN_4/bootservice/AutoStartUp.java
@@ -0,0 +1,22 @@
+package com.marcin.boot_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class AutoStartUp extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Serwis uruchomiony", Toast.LENGTH_SHORT).show();
+    }
+}
