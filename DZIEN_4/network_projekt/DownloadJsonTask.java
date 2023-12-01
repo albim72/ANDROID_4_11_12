d7777d35d76a19ded8ec2267ff0a57248461650c
albim72 <marcin.albiniak72@gmail.com>
2023-12-01 11:52:14 +0100

    Create DownloadJsonTask.java

---
+++ DZIEN_4/network_projekt/DownloadJsonTask.java
@@ -0,0 +1,67 @@
+package com.marcin.network;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DownloadJsonTask extends AsyncTask {
+    private TextView textView;
+    
+    @Override
+    protected Object doInBackground(Object[] params) {
+        String textUrl = (String)params[0];
+
+        InputStream in = null;
+        BufferedReader br = null;
+        try{
+            URL url = new URL(textUrl);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            
+            int resCode = httpConn.getResponseCode();
+            if (resCode == HttpURLConnection.HTTP_OK){
+                in = httpConn.getInputStream();
+                br = new BufferedReader(new InputStreamReader(in));
+                
+                StringBuilder sb = new StringBuilder();
+                String s = null;
+                while ((s=br.readLine()) != null){
+                    sb.append(s);
+                    sb.append("\n");
+                }
+                return sb.toString();
+            }else {
+                return null;
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(br);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Object result) {
+        if(result!=null){
+            this.textView.setText((String)result);
+        }else{
+            Log.e("MyMessage","Błąd!");
+                    
+        }
+    }
+}
+
