a4fd5dc7b2305d04a533c841c0f98faf70ab879b
albim72 <marcin.albiniak72@gmail.com>
2023-12-01 14:10:08 +0100

    Update MainActivity.java

--- DZIEN_4/restfullProject/MainActivity.java
+++ DZIEN_4/restfullProject/MainActivity.java
@@ -13,6 +13,20 @@ import android.widget.TextView;
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient;
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -43,8 +57,82 @@ public class MainActivity extends AppCompatActivity {
         EditText serverText = (EditText) findViewById(R.id.serverText);
 
         @Override
-        protected Void doInBackground(String... strings) {
+        protected void onPreExecute() {
+            Dialog.setMessage("Zaczekaj....");
+            Dialog.show();
+            
+            try{
+                data += "&" + URLEncoder.encode("data","UTF-8") + "=" + serverText.getText();
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        protected Void doInBackground(String... urls) {
+
+            BufferedReader reader = null;
+            try{
+                URL url = new URL(urls[0]);
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null){
+                    sb.append(line+"\n");
+                }
+                Content = sb.toString();
+                
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            finally {
+                try{
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            Dialog.dismiss();
+            if(Error!=null){
+                uiUpdate.setText("Informacja: " + Error);
+            }
+            else {
+                uiUpdate.setText(Content);
+                String OutputData = "";
+                JSONObject jsonResponse;
+                try{
+                    jsonResponse = new JSONObject(Content);
+                    JSONArray jsonMainNode = jsonResponse.optJSONArray("Android");
+                    int lengthJsonArr = jsonResponse.length();
+                    for(int i=0; i<lengthJsonArr;i++){
+                        JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                        
+                        String name = jsonChildNode.optString("name").toString();
+                        String number = jsonChildNode.optString("number").toString();
+                        String date_added = jsonChildNode.optString("date_added").toString();
+                        
+                        OutputData += " Name        :" + name +"\n"
+                                + " Number      :" + number + "\n"
+                                + " Time        :" + date_added + "\n__________________________________________\n";
+                    }
+                    jsonParsed.setText(OutputData);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
