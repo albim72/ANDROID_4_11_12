d6c85d27da8230372be7b248c43fe98029188930
albim72 <marcin.albiniak72@gmail.com>
2023-12-01 09:54:30 +0100

    Create GamesFragment.java

---
+++ DZIEN_4/TabsWithSwipeGesture/GamesFragment.java
@@ -0,0 +1,20 @@
+package com.marcin.tabswithswipegesture;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GamesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_games,container,false);
+        return rootView;
+    }
+}
